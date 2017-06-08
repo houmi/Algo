2d7af117ce2db6aa0761d348971d43e4502dd44f
Houmi <houman@pobox.com>
2017-06-08 11:50:55 -0700

    insert implementation

--- tree.cpp
+++ tree.cpp
@@ -5,26 +5,49 @@ using namespace std;
 class Tree {
     private:
 
-        typedef struct TreeNode {
+        struct TreeNode {
             TreeNode* left;
             TreeNode* right;
             int data;
-        } TreeNode;
-
-        TreeNode* root;
+        };
 
     public:
-        Tree() { root = NULL; }
+        Tree();
 
-        bool isEmpty() const { return root == NULL; }
+        bool isEmpty();
         void inOrderPrint();
         void postOrderPrint();
         void preOrderPrint();
-        void insert(int key, TreeNode *root);
+        TreeNode* insert(int key, TreeNode *root);
         TreeNode* search(int key, TreeNode *root);
         TreeNode* remove(int key, TreeNode* root);
 
 };
+
+Tree::TreeNode* Tree::insert(int key, TreeNode* root) {
+
+    if (root == NULL) {
+        TreeNode* node = new TreeNode;
+        node->left = NULL;
+        node->right = NULL;
+        node->data = key;
+        return node;
+    }
+
+    if (key < root->data) 
+        return(insert(key, root->left));
+    else
+        return(insert(key, root->right));
+
+}
+
+int main()
+{
+    return 0;
+}
+
+
+
 
 
 

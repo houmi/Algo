daa3556d2f3ed465dc6812aa22be5ae9aa948e2b
Houmi <houman@pobox.com>
2017-06-08 14:17:58 -0700

    adding deletetree, inorderprint & code cleanup

--- tree.cpp
+++ tree.cpp
@@ -5,44 +5,81 @@ using namespace std;
 class Tree {
     private:
 
-        struct TreeNode {
-            TreeNode* left;
-            TreeNode* right;
+        struct node {
+            node* left;
+            node* right;
             int data;
         };
 
-    public:
-        Tree();
+        node* _root;
 
+    public:
+        Tree() { _root = NULL; }
+        ~Tree() { _root = deleteTree(_root); }
         bool isEmpty();
-        void inOrderPrint();
-        void postOrderPrint();
-        void preOrderPrint();
-        TreeNode* insert(int key, TreeNode *root);
-        TreeNode* search(int key, TreeNode *root);
-        TreeNode* remove(int key, TreeNode* root);
+        void inOrderPrint(node* root);
+        void postOrderPrint(node* root);
+        void preOrderPrint(node* root);
+        node* deleteTree(node* root);
+        void insert(int key);
+        node* insert(int key, node* root);
+        node* search(int key, node* root);
+        node* remove(int key, node* root);
 
 };
 
-Tree::TreeNode* Tree::insert(int key, TreeNode* root) {
+/////////////////////////////////////////////////////////////////
+
+Tree::node* Tree::deleteTree(node* root) {
+    if (root) {
+        deleteTree(root->left);
+        deleteTree(root->right);
+        delete root;
+    }
+    return NULL;
+}
+
+void Tree::insert(int key) {
+    _root = insert(key, _root);
+}
+
+Tree::node* Tree::insert(int key, node* root) {
 
     if (root == NULL) {
-        TreeNode* node = new TreeNode;
-        node->left = NULL;
-        node->right = NULL;
-        node->data = key;
-        return node;
+        node* temp = new node;
+        temp->left = NULL;
+        temp->right = NULL;
+        temp->data = key;
+        return temp;
     }
 
     if (key < root->data) 
         return(insert(key, root->left));
     else
         return(insert(key, root->right));
-
 }
+
+void Tree::inOrderPrint(node* root) {
+    if (root) {
+        inOrderPrint(root->left);
+        cout << root->data;
+        inOrderPrint(root->right);
+    }
+}
+
 
 int main()
 {
+    Tree bst;
+    bst.insert(5);
+    bst.insert(4);
+    bst.insert(8);
+    bst.insert(2);
+    bst.insert(1);
+    
+
+    
+
     return 0;
 }
 

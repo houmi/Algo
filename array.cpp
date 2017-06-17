6bad3ec13d28ebedf61d4a5b93e3068f2df00bd5
Houmi <houman@pobox.com>
2017-06-16 22:57:16 -0700

    print array , remove dups methods

--- array.cpp
+++ array.cpp
@@ -2,12 +2,25 @@
 #include <vector>
 #include <map>
 #include <unordered_set>
+#include <algorithm>
 
 using namespace std;
 
+void printArray(vector<int> arr) {
+    cout << "Print vector" << endl;
+    string delim = "";
+    for (auto num : arr) {
+        cout << delim << num;
+        delim = ",";
+    }
+    cout << endl;
+
+}
 
 void findPairSum(vector<int> arr, int k) {
     unordered_set<int> set;
+
+    cout << "Find Pair Sum" << endl;
 
     for (auto num : arr) {
         int target = k - num;
@@ -17,14 +30,33 @@ void findPairSum(vector<int> arr, int k) {
             set.insert(num);
         }
     }
-
 }
+
+void removeDuplicates(vector<int> arr) {
+    vector<int> arr2(arr);
+    sort(arr2.begin(), arr2.end());
+
+    int idx = 0;
+    for (int i=1; i<arr2.size();i++) {
+        if (arr2[i] != arr2[idx]) {
+            arr2[++idx] = arr2[i];
+        }
+    }
+    
+    arr2.resize(idx);
+    printArray(arr2);
+}
+
+
 
 int main() {
 
     vector<int> arr = {50,3,2,-9, 10,9,45,-40, 123,2,3,1,2,5,9,8,-11,25,30,10,19,3};
 
+    printArray(arr);
     findPairSum(arr, 13);
+    removeDuplicates(arr);
+
 
     
     return 0;

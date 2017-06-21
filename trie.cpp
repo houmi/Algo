546a5ce255a2365d7231f36ff7b5a12e1c318d2d
Houmi <houman@pobox.com>
2017-06-20 20:56:13 -0700

    -core dump fixed -print issues...

--- trie.cpp
+++ trie.cpp
@@ -5,34 +5,52 @@
 using namespace std;
 
 struct node {
-    vector<node*> children;   
+    node* children[26];
 };
 
+node* getnode() {
+    node* newnode = nullptr;
+    newnode = new node;
+    if (newnode) {
+        for (int i=0; i<26;i++) {
+            newnode->children[i] = nullptr;
+        }
+    }
+    return newnode;
+}
 
+void printall(node *root) {
+
+    for (int i=0; i<26;i++) {
+        if (root->children[i]) {
+            printall(root->children[i]);
+            cout << (char)(i+'a') << endl;
+        }
+    }
+}
 
 void insert(node* root, string str) {
     node* cur = root;
     for (char ch: str) {
-        int index = ch - '0';
+        int index = tolower(ch) - 'a';
         if (cur->children[index] == nullptr) {
-            node* newnode = new node;
+            node* newnode = getnode();
             cur->children[index] = newnode;
         } 
 
         cur = cur->children[index];
     }
-
-    
+  
 }
 
 
 
-
 int main() {
-    node* trie = new node;
+    node* trie = getnode();       
+    vector<string> str = {"noob", "max", "man", "mix", "groom"};
+    for (auto s:str) {
+       insert(trie, s);
+    }
 
-    string str = "noob";
-    insert(trie, str);
-
-
+    printall(trie);
 }

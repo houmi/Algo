8c2203a998f209e850944113a13ef5ba773f64d1
Houmi <houman@pobox.com>
2017-06-21 17:42:04 -0700

    changed print all, still issues

--- trie.cpp
+++ trie.cpp
@@ -5,13 +5,15 @@
 using namespace std;
 
 struct node {
+    int level;
     node* children[26];
 };
 
-node* getnode() {
+node* getnode(int level) {
     node* newnode = nullptr;
     newnode = new node;
     if (newnode) {
+        newnode->level = level;
         for (int i=0; i<26;i++) {
             newnode->children[i] = nullptr;
         }
@@ -19,22 +21,40 @@ node* getnode() {
     return newnode;
 }
 
-void printall(node *root) {
-
+void helperprint(node* root, vector<string>& words, string &word) {
+    
     for (int i=0; i<26;i++) {
         if (root->children[i]) {
-            printall(root->children[i]);
-            cout << (char)(i+'a') << endl;
+            word += (char)(i+'a');
+            helperprint(root->children[i], words, word);
+            words.push_back(word);
+            word.pop_back();          
         }
     }
 }
 
+void printall(node *root) {
+
+    string word = "";
+    vector<string> words;
+
+    helperprint(root, words, word);
+
+    for (auto word : words) {
+        cout << word << endl;
+    }
+
+    
+}
+
 void insert(node* root, string str) {
     node* cur = root;
+    int level = 1;
     for (char ch: str) {
         int index = tolower(ch) - 'a';
         if (cur->children[index] == nullptr) {
-            node* newnode = getnode();
+            node* newnode = getnode(level);
+            level++;
             cur->children[index] = newnode;
         } 
 
@@ -46,11 +66,13 @@ void insert(node* root, string str) {
 
 
 int main() {
-    node* trie = getnode();       
+    node* trie = getnode(0);       
     vector<string> str = {"noob", "max", "man", "mix", "groom"};
     for (auto s:str) {
        insert(trie, s);
     }
 
+
     printall(trie);
+    return 0;
 }

d0a1768def53f8161d54ffc05651d3210cec720b
Houmi <houman@pobox.com>
2017-06-23 11:54:32 -0700

    - add is word to node to mark end of words - print needs to change to read isWord correctly from the correct child.

--- trie.cpp
+++ trie.cpp
@@ -5,18 +5,18 @@
 using namespace std;
 
 struct node {
-    int level;
+    bool isWord;
     node* children[26];
 };
 
-node* getnode(int level) {
+node* getnode() {
     node* newnode = nullptr;
     newnode = new node;
     if (newnode) {
-        newnode->level = level;
         for (int i=0; i<26;i++) {
             newnode->children[i] = nullptr;
         }
+        newnode->isWord = false;
     }
     return newnode;
 }
@@ -27,7 +27,9 @@ void helperprint(node* root, vector<string>& words, string &word) {
         if (root->children[i]) {
             word += (char)(i+'a');
             helperprint(root->children[i], words, word);
-            words.push_back(word);
+            if (root->children[i]->isWord) {
+                words.push_back(word);
+            }
             word.pop_back();          
         }
     }
@@ -49,25 +51,24 @@ void printall(node *root) {
 
 void insert(node* root, string str) {
     node* cur = root;
-    int level = 1;
+    int index;
     for (char ch: str) {
-        int index = tolower(ch) - 'a';
+        index = tolower(ch) - 'a';
         if (cur->children[index] == nullptr) {
-            node* newnode = getnode(level);
-            level++;
+            node* newnode = getnode();
             cur->children[index] = newnode;
         } 
 
         cur = cur->children[index];
     }
-  
+    cur->isWord = true;
 }
 
 
 
 int main() {
-    node* trie = getnode(0);       
-    vector<string> str = {"noob", "max", "man", "mix", "groom"};
+    node* trie = getnode();       
+    vector<string> str = {"noob", "max", "man", "mix", "grooming", "groom"};
     for (auto s:str) {
        insert(trie, s);
     }

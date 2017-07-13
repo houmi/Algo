f9e39630ac55daef014bcd54f4bbb86dfe5ecfe5
Houmi <houman@pobox.com>
2017-07-12 22:14:26 -0700

    code clean up and more debugging added

--- findPreviousChar.cpp
+++ findPreviousChar.cpp
@@ -11,7 +11,7 @@ char* findPrev(const char* start, char* ptr) {
         res = nullptr;
     } else if (ptr-1 == start) {
         res = ptr-1;
-    } else if (!(*ptr & 0x80)) {
+    } else if (!(*(ptr-2) & 0x80)) {
         res = ptr-1;
     } else {
         res = ptr-2;
@@ -21,6 +21,14 @@ char* findPrev(const char* start, char* ptr) {
     
 }
 
+printRes(char* start, char* ptr, char* res) {
+     if (res) {
+        printf("previous character from %c (index=%d) is %c (index = %d)\n", *ptr, ptr-start, *res, res-start);
+    } else {
+        printf("no previous from %c (index=%d) \n", *ptr, ptr-start);
+    }
+}
+
 
 int main() {
     
@@ -28,51 +36,36 @@ int main() {
     printf("%s\n",str);
     char* start = str;
     while (*start != '\0') {
-        if (*start & 0x80) {
-            printf("%c: ", *start);
+        if ((*start & 0x80) != 0) {
+            cout << *start << " : ";
             bitset<8> x(*start);
-            bitset<8> y(*start+1);
+            start++;
+            bitset<8> y(*start);
             cout << x << " " << y << endl;
-            start +=2;
+            start++;
         } else {
-            printf("%c: ", *start);
+            cout << *start << " : ";
             bitset<8> x(*start);
             cout << x << endl;
-            start++;
+            start ++;
         }
     }
     
     char* ptr = str+5;
     char* res = findPrev(str, ptr);
-    if (res) {
-        printf("previous character from %c is %c\n", *ptr, *res);
-    } else {
-        printf("no previous\n");
-    }
-
+    printRes(str, ptr, res);
+   
     ptr = str;
     res = findPrev(str, ptr);
-    if (res) {
-        printf("previous character from %c is %c\n", *ptr, *res);
-    } else {
-        printf("no previous \n");
-    }
+    printRes(str, ptr, res);
 
     ptr = str+4;
     res = findPrev(str, ptr);
-    if (res) {
-        printf("previous character from %c is %c\n", *ptr, *res);
-    } else {
-        printf("no previous \n");
-    }
+    printRes(str, ptr, res);
 
-    ptr = str+1;
+    ptr = str+2;
     res = findPrev(str, ptr);
-    if (res) {
-        printf("previous character from %c is %c\n", *ptr, *res);
-    } else {
-        printf("no previous \n");
-    }
+    printRes(str, ptr, res);
 
     return 0;
 }

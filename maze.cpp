6a57dcb97255651ab96761824d2c2bc4e2f5b494
Houmi <houman@pobox.com>
2017-07-22 15:43:51 -0700

    find path

--- maze.cpp
+++ maze.cpp
@@ -43,6 +43,54 @@ int numberOfPaths(vector<vector<int>> grid, int row, int col, vector<vector<int>
     
 }
 
+bool findPaths(vector<vector<int>> grid, int row, int col, vector<vector<int>> sol, vector<vector<vector<int>>>& grids) {
+    if (isAtEnd(grid, row, col)) {
+        sol[row][col]=1;
+        grids.push_back(sol);
+        return true;
+    }
+
+    if (isValid(grid, row, col)) {
+
+        sol[row][col]=1;
+
+        if (isValid(grid, row+1, col)) {
+            return (findPaths(grid, row+1, col, sol, grids));
+        }
+        
+        if (isValid(grid, row, col+1)) {
+            return (findPaths(grid, row, col+1, sol, grids));
+        }
+
+        sol[row][col]=0;
+    }
+
+    return false;
+}
+
+void printMaze(vector<vector<int>> grid) {
+
+    for (auto row : grid) {
+        for (auto num : row) {
+            cout << num;
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+}
+
+void printMazes(vector<vector<vector<int>>> grids) {
+    for (auto grid : grids) { 
+        for (auto row : grid) {
+            for (auto num : row) {
+                cout << num;
+            }
+            cout << endl;
+        }
+    }
+}
+
 
 int main() {
 
@@ -54,6 +102,8 @@ int main() {
                                  { 0, 0, 0, 1, 1, 0, 1, 0 } ,
                                  { 0, 1, 0, 0, 0, 1, 0, 0 } ,
                                  { 0, 0, 0, 0, 0, 0, 0, 0 }};
+    cout << "Maze:" << endl;
+    printMaze(maze);
 
     vector<vector<int>> paths(8, vector<int>(8,0));
     
@@ -64,6 +114,11 @@ int main() {
 
     numpaths = numberOfPaths(maze, 0, 0, paths);
     cout << "Number of Paths (with memorization): " << numpaths << endl;
+
+    vector<vector<vector<int>>> grids;
+    findPaths(maze, 0, 0, paths, grids);
+    cout << "Find Path:" << endl;
+    printMazes(grids);
 
     return 0;
 
